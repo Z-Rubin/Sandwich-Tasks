ce7a89f1d220e82e865bfe3e1d8239171dcf0f6f
Z-Rubin <86024631+Z-Rubin@users.noreply.github.com>
2024-01-19 10:44:29 +0200

    Initial Commit

---
+++ Countdown Timer/Countdown Timer/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Threading;
+
+
+class Countdown_Timer
+{
+
+    static void Countdown_Sleep (int N, int step)
+    {
+        for (int i = N; i >= 0; i-=step)
+        {
+            Console.WriteLine(i);
+            if (N != 0)
+            {
+                Thread.Sleep(step * 1000);
+            }
+
+        }
+    }
+
+    static void Main()
+    {
+        Console.WriteLine("Input an integer to count down from:");
+        string sN = Console.ReadLine();
+        try
+        {
+            int N = int.Parse(sN);
+
+            Console.WriteLine("Input an integer for step size:");
+            string sStep = Console.ReadLine();
+            int Step = int.Parse(sStep);
+            Countdown_Sleep(N, Step);
+        }
+        catch
+        {
+            Console.WriteLine("Input must be an integer. Close the program and try again.");
+
+        }
+    }
+
+}
---
+++ Simple Calculator/Simple Calculator/Program.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+class Simple_Calculator
+{
+
+
+    static char[] Add_Sub = { '+', '-' };
+    static char[] Times_Div = { '*', '/' };
+
+
+
+    static double Evaluate(string s)
+    {
+        return Split_Add_Sub(s);
+    }
+
+    static double Split_Add_Sub(string s)
+    {
+        string sub_s;
+        string eval_string;
+        string temp_s;
+        double sum = 0;
+        int pos = s.IndexOfAny(Add_Sub);
+
+        if (pos == -1)
+        {
+            if (s.IndexOfAny(Times_Div) == -1)
+            {
+                sum = double.Parse(s);
+            }
+            else
+            {
+            sum = Solve_Times_Div(s);
+            }
+        }
+        else
+        {
+            if (s.Substring(0,1).IndexOfAny(Add_Sub) == -1)
+            {
+                s = "+" + s;
+            }
+
+            while (s.Length > 0)
+            {
+                pos = s.Substring(1,s.Length-1).IndexOfAny(Add_Sub)+1;
+                if (pos == 0)
+                {
+                    sub_s = s;
+                    pos = s.Length;
+                }
+                else
+                {
+                    sub_s = s.Substring(0, pos);
+                }
+
+      
+
+                eval_string = s.Substring(1, pos - 1);
+                Console.WriteLine(eval_string);
+
+                if (sub_s.IndexOfAny(Times_Div) == -1)
+                {
+                    if (sub_s[0] == '+')
+                    {
+                        sum += double.Parse(eval_string);
+                    }
+                    else
+                    {
+                        sum -= double.Parse(eval_string);
+                    }
+              
+                }
+                else
+                {
+                    if (sub_s[0] == '+')
+                    {
+                        sum += Solve_Times_Div(eval_string);
+                    }
+                    else
+                    {
+                        sum -= Solve_Times_Div(eval_string);
+                    }
+                  
+                }
+                s = s.Substring(pos);
+
+
+
+            }
+        }
+
+
+        return sum;
+    }
+    static double Solve_Times_Div(string s)
+    {
+        double total = 0;
+        int pos = s.IndexOfAny(Times_Div);
+
+
+        total = double.Parse(s.Substring(0, pos));
+        s = s.Substring(pos);
+
+
+        while (Times_Div.Any(c => s.Contains(c))) {
+            pos = s.IndexOfAny(Times_Div);
+
+            if (s[pos] == '*')
+            {
+                s = s.Substring(pos+1);
+                pos = s.IndexOfAny(Times_Div);
+                if (pos != -1)
+                {
+                    total *= double.Parse(s.Substring(0, pos));
+                } else
+                {
+                    total *= double.Parse(s);
+                }
+            }
+            else
+            {
+                s = s.Substring(pos+1);
+                pos = s.IndexOfAny(Times_Div);
+                if (pos != -1)
+                {
+                    total /= double.Parse(s.Substring(0, pos));
+                }
+                else
+                {
+                    total /= double.Parse(s);
+                }
+ 
+            }
+        }
+        return total;
+    }
+    static void Main()
+    {
+        while (true)
+        {
+            string equation;
+            Console.WriteLine("Input a mathematical equation to calculate:");
+            
+            equation  = Console.ReadLine();
+            double output = Evaluate(equation);
+            //double output = Solve_Times_Div(equation);
+            Console.WriteLine("Result:");
+            Console.WriteLine(output);
+     
+
+
+
+        }
+
+
+    }
+
+
+}
