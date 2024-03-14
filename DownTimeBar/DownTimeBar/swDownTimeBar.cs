fc89639bc559ca2b8d6bb3961349c32f745f58c8
Zac-Sandwich <zac@sandwichtrading.com>
2024-03-14 13:41:32 +0200

    Created downtimebar to test

---
+++ DownTimeBar/DownTimeBar/swDownTimeBar.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sandwich.CustomControls
+{
+    public partial class swDownTimeBar : UserControl
+    {
+        public swDownTimeBar()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+            e.Graphics.DrawRectangle(new Pen(ForeColor), ClientRectangle);
+        }
+
+
+    }
+
+
+}
